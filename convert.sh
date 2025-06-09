fb4dbb5d3603334710f4c2f4a29397015975d139
Keith Pitt <git@keithpitt.email>
2025-06-09 11:08:41 +0800

    Added docker and fixed convert script

--- convert.sh
+++ convert.sh
@@ -5,7 +5,6 @@ name="$(basename $path)"
 url="$2"
 
 magick "icons/$path.png" -background transparent -resize 300x300 -gravity center -extent 300x300 "icons/$path-resized.png"
-out=$(ascii-silhouettify -i "icons/$1.png")
+out=$(ascii-silhouettify -i "icons/$1-resized.png")
 echo "$out"
-echo "$out" > "icons/$1/$1.png"
-
+echo "$out" > "icons/$path.ansi"
